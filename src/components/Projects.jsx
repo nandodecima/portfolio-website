eb51f91ccc7ec5d459b6ae1b2cc6374504d63431
nandodecima <hnandodecima@gmail.com>
2025-03-14 09:57:11 -0700

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,6 +9,11 @@ const Projects = () => {
       link: 'https://nandodecima-portfolio.netlify.app/',
     },
     {
+      title: 'Space Battle Simulator',
+      description: 'Space Battle Simulator is a simple yet fun web-based game where players and enemies exchange attacks at random with the press of a button. Each battle unfolds unpredictably, making every round a unique experience. This project highlights basic game logic, user interaction, and randomness in JavaScript.',
+      extra:'https://space-battle-simulator.netlify.app/'
+    },
+    {
       title: 'Gloss – Music Collaboration Platform',
       description: 'Gloss is a web-based platform designed to help musicians, producers, and audio engineers connect and collaborate effortlessly. Users can upload tracks, exchange ideas, and work together in real-time, breaking down the barriers of remote music creation. With an intuitive interface and streamlined workflow, Gloss makes it easy to bring creative visions to life, whether you’re co-producing a song, giving feedback, or building something new with other artists.',
       extra:'Coming Soon!'
